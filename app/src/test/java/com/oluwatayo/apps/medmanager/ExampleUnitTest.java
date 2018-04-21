ee38d1fa977f955203b0c9facbc934f6474c6a3b
oluwatayo <oluwatayoadegboye@gmail.com>
2018-04-21 13:42:17 +0100

    fix bug intest

--- app/src/test/java/com/oluwatayo/apps/medmanager/ExampleUnitTest.java
+++ app/src/test/java/com/oluwatayo/apps/medmanager/ExampleUnitTest.java
@@ -14,10 +14,6 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
 
     @Test
     public void dateCorrect()throws Exception{
